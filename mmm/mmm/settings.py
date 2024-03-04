bcf08e7d92f8d337b2ec392565b3a2766f2c9cd2
YuriiSmakota <yurii.smakota@gmail.com>
2024-03-04 19:37:40 +0200

    mmm_v0.0.1 start-app + postgresql

--- mmm/mmm/settings.py
+++ mmm/mmm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mmm_app.apps.MmmAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -72,14 +73,17 @@ WSGI_APPLICATION = 'mmm.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+#
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
