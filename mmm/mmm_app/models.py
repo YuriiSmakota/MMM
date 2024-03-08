d22c0741b7cbb8b14b939a6da8440ed8191db2d7
YuriiSmakota <yurii.smakota@gmail.com>
2024-03-08 13:48:06 +0200

    mmm_v0.0.1 pg+model(user)+admin

--- mmm/mmm_app/models.py
+++ mmm/mmm_app/models.py
@@ -1,3 +1,11 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    login = models.CharField(max_length=16, unique=True, null=False)
+    password = models.CharField(max_length=16)
+    email = models.EmailField()
+    status = models.CharField()
